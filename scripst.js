17c51249893de01315d7491bd0ef76f33d6d79bf
AdriellyKerolyn <adriellykerolyn@gmail.com>
2026-02-04 13:54:15 -0300

    Estrutura correta de pastas

--- scripst.js
+++
@@ -1,6 +0,0 @@
-// 1. Eu seleciono os elementos que vou manipular no DOM
-const campoArquivo = document.getElementById('arquivo');
-const selectAgrupamento = document.getElementById('coluna-agrupadora');
-const btnGerar = document.getElementById('gerar-relatorio');
-const statusMsg = document.getElementById('status-processamento');
-const downloadContainer = document.getElementById('download-link');
